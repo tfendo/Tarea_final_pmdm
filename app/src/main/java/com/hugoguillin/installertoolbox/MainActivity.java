d2899f1641d6519676985a53a99882236b058444
Hugo Guillín <hugoguillin@hotmail.com>
2019-04-27 17:38:34 +0200

    Cargadas acciones en pantalla principal

--- app/src/main/java/com/hugoguillin/installertoolbox/MainActivity.java
+++ app/src/main/java/com/hugoguillin/installertoolbox/MainActivity.java
@@ -4,12 +4,20 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private ArrayList<Acciones> datosAcciones;
+    private AccionesAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +25,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        datosAcciones = new ArrayList<>();
+        adapter = new AccionesAdapter(this, datosAcciones);
+        recyclerView.setAdapter(adapter);
+        iniciarDatos();
+    }
 
+    private void iniciarDatos(){
+        String[] listaAcciones = getResources().getStringArray(R.array.acciones);
+        String[] listaDescripciones = getResources().getStringArray(R.array.descripcion_acciones);
 
+        datosAcciones.clear();
+
+        for (int i = 0; i<listaAcciones.length; i++){
+            datosAcciones.add(new Acciones(listaAcciones[i], listaDescripciones[i]));
+        }
+
+        adapter.notifyDataSetChanged();
     }
 
     @Override
